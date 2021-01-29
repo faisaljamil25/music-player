7676b1c1ce710fe3559aa0d74d26d8b69ed6ed5c
Faisal Jamil <faisaljamilcs@gmail.com>
2021-01-29 21:21:27 +0530

    feat: added songs

--- src/util.js
+++ src/util.js
@@ -13,6 +13,16 @@ function chillHop() {
       active: true,
     },
     {
+      name: "Rest Until Dark",
+      artist: "Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10015",
+      color: ["#36A3BD", "#FEEDD8"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "Hereafter",
       artist: "Makzo",
       cover:
@@ -29,6 +39,56 @@ function chillHop() {
         "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11770",
       color: ["#478965", "#D49064"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ocean View",
+      artist: "G Mills, Kyle McEvoy",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10320",
+      color: ["#C9AFC8", "#1F487C"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Going Back",
+      artist: "Swørn",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10310",
+      color: ["#335561", "#404048"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Canary Forest",
+      artist: "Aso, Middle School, Aviino",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#B39E6C", "#08373F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Sugarless",
+      artist: "The Field Tapes, Aviino",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11243",
+      color: ["#404654", "#B3BC85"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Finally Home",
+      artist: "Sitting Duck, Hoffy Beats",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8095",
+      color: ["#4D5C41", "#BFAB79"],
       id: uuidv4(),
       active: false,
     },
